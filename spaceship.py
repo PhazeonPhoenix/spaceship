eca50c209be630ca5c029f6f77d66f2aa67f56f7
Phazeon Phoenix <phoenix@phazeon.com>
2012-09-16 18:09:29 -0700

    Filled in a little documentation.

--- spaceship.py
+++ spaceship.py
@@ -1,10 +1,38 @@
+"""
+The vast universe, in code.
+
+'Fascinating'  - Spock
+
+"""
+
+
 class Space(list):
-    """The vast emptiness of space."""
+    """
+    The vast emptiness of space.
+
+    Anything that might contain a Thing with a coordinate might go here.
+    It could be an empty void of space, it could be in local space around
+    the Earth or it could be the space surrounding a star, black hole or other
+    such bodies. The planets and stars would also require Things to represent
+    them in their own surrounding space.
+    """
     def __init__(self, name=""):
+        """
+        Create empty space.
+
+        Giving a name to a vast stretch of nothingness seems to make it
+        something.
+        """
         super(Space, self).__init__()
         self.name = name
 
     def __str__(self):
+        """
+        Output some information.
+
+        For being nothing, there certainly seems to be a lot of things to say
+        about it.
+        """
         output = "Space: \'{}\'".format(self.name)
         if self:
             output += " contains:\n"
@@ -16,22 +44,29 @@ class Space(list):
 class Coordinate:
     """Represents a coordinate in 2d space"""
     def __init__(self, x=None, y=None):
+        """Nothing too fancy, just x and y"""
         self.x = x
         self.y = y
 
     def __str__(self):
-        return "{}, {}".format(self.x, self.y)
+        """Output in standard (x,y) format"""
+        return "({}, {})".format(self.x, self.y)
 
 
 class Thing:
     """The things that fill the void"""
     def __init__(self, name="", location=None, x=None, y=None, ):
+        """
+            Create the thing from nothing, give it a name and put give it
+            an imaginary place in imaginary space.
+        """
         if x and y and not location:
             location = Coordinate(x=x, y=y)
         self.location = location
         self.name = name
 
     def __str__(self):
+        """Output what I am, who I am and where I am."""
         return "Thing: \'{}\' at {!s}".format(self.name, self.location)
 
 if __name__ == "__main__":
